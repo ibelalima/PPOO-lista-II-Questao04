48a0bc610075e1b5cfc6d1c0ce42743e2bd1dc73
Isabela <isabela.franca.lima@gmail.com>
2025-11-17 13:46:35 -0300

    Estrutura do projeto

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Produto produto = new Produto(1, "Fone Bluetooth");
+        PedidoFacade pedidoFacade = new PedidoFacade();
+
+        boolean resultado = pedidoFacade.processarPedido(produto);
+
+        if(resultado){
+            System.out.println("Fluxo concluído!");
+        } else {
+            System.out.println("Falha no processamento.");
+        }
+    }
+}

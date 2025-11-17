04ae4e7f22641d00acf37b09afbb449fb74b303b
Isabela <isabela.franca.lima@gmail.com>
2025-11-17 13:58:56 -0300

    Implementar serviços de estoque, pagamento e remessa

--- ServicoEstoque.java
+++ ServicoEstoque.java
@@ -1,3 +1,7 @@
 public class ServicoEstoque {
-    
+    public static boolean isAvailable(Produto product){
+        System.out.println("Verificando estoque do produto: " + product.name);
+        return true;
+    }
 }
+
--- ServicoPagamento.java
+++ ServicoPagamento.java
@@ -1,3 +1,7 @@
 public class ServicoPagamento {
-    
+    public static boolean makePayment(){
+        System.out.println("Processando pagamento...");
+        return true;
+    }
 }
+

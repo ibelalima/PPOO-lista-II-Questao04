d03b050a338648f02334ead7300ed00541a5d911
Isabela <isabela.franca.lima@gmail.com>
2025-11-17 14:03:20 -0300

    Implementar PedidoFacade

--- PedidoFacade.java
+++ PedidoFacade.java
@@ -1,3 +1,21 @@
 public class PedidoFacade {
-    
+
+    public boolean processarPedido(Produto produto){
+        System.out.println("Iniciando processamento do pedido...");
+
+        if(!ServicoEstoque.isAvailable(produto)){
+            System.out.println("Produto indisponível no estoque.");
+            return false;
+        }
+
+        if(!ServicoPagamento.makePayment()){
+            System.out.println("Falha no pagamento.");
+            return false;
+        }
+
+        ServicoRemessa.shipProduct(produto);
+
+        System.out.println("Pedido processado com sucesso!");
+        return true;
+    }
 }
